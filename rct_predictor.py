0b29145e50e721b40b840afd0b4e76cebd0538cd
Juan Vásquez <ji.vasquez0@gmail.com>
2019-04-09 15:51:58 +1000

    Fixed headers

--- rct_predictor.py
+++ rct_predictor.py
@@ -7,19 +7,20 @@ rct_clf = rct_robot.RCTRobot()
 
 
 def main():
-    print(sys.argv[1])
     for index, line in enumerate(open(sys.argv[1], mode='rt', encoding='utf-8')):
         splitted_line = line.rstrip("\r\n").split('\t')
-        if len(splitted_line) == 0:
-            continue
-        try:
-            title = splitted_line[1]
-            abstract = splitted_line[2]
-            pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
-            splitted_line.append(str(pred[0]["is_rct"]))
+        if index == 0:
+            splitted_line.append("is_rct")
             print(("\t".join(splitted_line)))
-        except:
-            raise Exception("There's been a problem")
+        else:
+            try:
+                title = splitted_line[1]
+                abstract = splitted_line[2]
+                pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
+                splitted_line.append(str(pred[0]["is_rct"]))
+                print(("\t".join(splitted_line)))
+            except:
+                raise Exception("There's been a problem")
 
 
 if __name__ == "__main__":
