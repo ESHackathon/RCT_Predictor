4b26c3091eb48c0caa54859a212d3435213553f2
Matt Carter <m@ttcarter.com>
2019-04-09 14:43:17 +1000

    BUGFIX: Attemping to fix ASCII issues

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ RUN apt-get update -y
 RUN apt-get install python3.6 python3-pip python3-dev -y
 RUN apt-get install curl -y
 WORKDIR /app
-COPY rct_predictor.py requirements.txt /app/
 RUN pip3 install setuptools==41.0.0
 RUN pip3 install -U pip wheel
-RUN pip3 install -r requirements.txt
+# RUN pip3 install -r requirements.txt
+RUN pip3 install https://github.com/ijmarshall/robotsearch/archive/master.zip
+COPY rct_predictor.py requirements.txt /app/
 # RUN pip3 install https://github.com/ijmarshall/robotsearch/archive/master.zip
 ENTRYPOINT ["/usr/bin/python3", "rct_predictor.py"]
--- rct_predictor.py
+++ rct_predictor.py
@@ -8,9 +8,13 @@ rct_clf = rct_robot.RCTRobot()
 
 
 def main():
-    with open(sys.argv[1]) as f:
+    print(sys.argv[1])
+    with open(sys.argv[1], mode='rt', encoding='utf-8') as f:
         for line in f:
             splitted_line = line.replace("\n", "").split('\t')
+            if not splitted_line:
+                continue
+            print('Working with line', line)
             title = splitted_line[1]
             abstract = splitted_line[2]
             pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
