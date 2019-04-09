5dc5b8a68616d4522c11f4c8880d33978431b73d
Juan Vásquez <ji.vasquez0@gmail.com>
2019-04-09 15:42:40 +1000

    Solved ascii problem

--- rct_predictor.py
+++ rct_predictor.py
@@ -1,4 +1,3 @@
-
 import sys
 
 from robotsearch.robots import rct_robot
@@ -9,17 +8,18 @@ rct_clf = rct_robot.RCTRobot()
 
 def main():
     print(sys.argv[1])
-    with open(sys.argv[1], mode='rt', encoding='utf-8') as f:
-        for line in f:
-            splitted_line = line.replace("\n", "").split('\t')
-            if not splitted_line:
-                continue
-            print('Working with line', line)
+    for index, line in enumerate(open(sys.argv[1], mode='rt', encoding='utf-8')):
+        splitted_line = line.rstrip("\r\n").split('\t')
+        if len(splitted_line) == 0:
+            continue
+        try:
             title = splitted_line[1]
             abstract = splitted_line[2]
             pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
             splitted_line.append(str(pred[0]["is_rct"]))
-            print("\t".join(splitted_line))
+            print(("\t".join(splitted_line)))
+        except:
+            raise Exception("There's been a problem")
 
 
 if __name__ == "__main__":
