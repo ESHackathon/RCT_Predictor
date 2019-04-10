912f6bf2b9ccb4fdcf9174d775eb9da594fd0a20
pmartin23 <paige.newman@mq.edu.au>
2019-04-10 10:11:04 +1000

    BUGFIX: read and output file correctly

--- rct_predictor.py
+++ rct_predictor.py
@@ -1,29 +1,26 @@
 import sys
 
 from robotsearch.robots import rct_robot
+import csv
 
 
 rct_clf = rct_robot.RCTRobot()
 
 
 def main():
-    for index, line in enumerate(open(sys.argv[1], mode='rt', encoding='utf-8')):
-        splitted_line = line.rstrip("\r\n").split('\t')
-        # skip blank lines
-        if len(splitted_line) < 2:
-            continue
-        if index == 0:
-            splitted_line.append("is_rct")
-            print(("\t".join(splitted_line)))
-        else:
-            try:
-                title = splitted_line[1]
-                abstract = splitted_line[2]
-                pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
-                splitted_line.append(str(pred[0]["is_rct"]))
-                print(("\t".join(splitted_line)))
-            except:
-                raise Exception("There's been a problem")
+    with open(sys.argv[1]) as tsvfile:
+        reader = csv.reader(tsvfile, delimiter='\t')
+        header = next(reader)
+        header.append('is_rct')
+        print("\t".join(header))
+        for line in reader:
+            if len(line) < 3:
+                continue
+            title = line[1]
+            abstract = line[2]
+            pred = rct_clf.predict({"title": title, "abstract": abstract, "use_ptyp": False}, filter_type="balanced", filter_class="svm_cnn")
+            line.append(str(pred[0]["is_rct"]))
+            print(("\t".join(line).replace('\n',' ')))
 
 
 if __name__ == "__main__":
